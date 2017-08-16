2e976149dafe3309bb57d7e48c2550247975bded
Domin536 <domin536k@gmail.com>
2017-08-16 11:13:08 +0200

    Final version

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,31 @@ $(window).scroll(function() {
     }
 });
 
+/*!
+ * jQuery Cookie Policy Plugin
+ */
+(function($) {
+$.fn.cookiepolicy = function(options) {
+    new jQuery.cookiepolicy($(this), options);
+  return this;
+};
+
+$.cookiepolicy = function(options) {
+  options = $.extend({
+    cookie: 'cookiepolicyinfo',
+    info: 'Korzystanie z witryny oznacza zgodę na wykorzystywanie plików cookie.',
+    close: 'Zamknij'
+  }, options || {});
+  
+    if($.cookie(options.cookie) != 'true') {
+        var wrapper = $('<div/>').addClass('cookiepolicy').appendTo('body');
+        $('<span/>').html(options.info).appendTo(wrapper);
+        $('<a/>').addClass('button').html(options.close).appendTo(wrapper)
+            .on('click', function(e) {
+                e.preventDefault();
+                $.cookie(options.cookie, true);
+                $(this).parents('.cookiepolicy').remove();
+            });
+    }
+};
+})(jQuery);
