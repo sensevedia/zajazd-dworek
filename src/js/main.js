0785b134776041884a45d1e77dc862d447117a66
Domin536 <domin536k@gmail.com>
2017-08-22 17:17:50 +0200

    tidy

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,14 @@ $(window).scroll(function() {
     if ($(".navbar-fixed-top").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
         
-        //$(".container-nav").removeClass("scroll-nav");
-       // $(".container-nav").addClass("scroll-nav-desktop");
-       // $("body").addClass("small-padding");
+      
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
        
-      //  $(".container-nav").addClass("scroll-nav");
-       // $(".container-nav").removeClass("scroll-nav-desktop");
-       //  $("body").removeClass("small-padding");
     }
 });
 
-/*!
- * jQuery Cookie Policy Plugin
- */
+
 (function($) {
 $.fn.cookiepolicy = function(options) {
     new jQuery.cookiepolicy($(this), options);
